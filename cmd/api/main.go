45676b0559d2f16b412b300cb8c058bac4f85e4a
Keith Crooks <keithcrooks@gmail.com>
2025-11-16 22:42:56 +0000

    Module 4: Adding a Database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/keithcrooks/readinglist/internal/data"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,11 +18,13 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	dsn  string
 }
 
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -26,13 +32,29 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod)")
+	flag.StringVar(&cfg.dsn, "db-dsn", os.Getenv("READINGLIST_DB_DSN"), "Postgres DSN")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	db, err := sql.Open("postgres", cfg.dsn)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Printf("database connection pool established")
+
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	addr := fmt.Sprintf(":%d", cfg.port)
@@ -47,6 +69,6 @@ func main() {
 
 	logger.Printf("starting %s server on %s", cfg.env, addr)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/keithcrooks/readinglist
 
 go 1.25.3
+
+require github.com/lib/pq v1.10.9 // indirect
